4eea7abe805ea270ba015bc84cbee88a2d5a3146
Riccardo Mattesini <da.ri.mat42@gmail.com>
2021-05-25 18:51:08 +0200

    Update ETM.c

--- nfc-bluetooth/controllers/test_comms_position/ETM.c
+++ nfc-bluetooth/controllers/test_comms_position/ETM.c
@@ -4,4 +4,17 @@
 
 #include "ETM.h"
 
-void 
+float retrieve_closest_external_connection(void) {
+  
+  float x_ext, y_ext;
+  
+  return x_ext, y_ext;
+  
+}
+
+void redirect_bot_to_team(void) {
+  
+  return;
+  
+}
+
